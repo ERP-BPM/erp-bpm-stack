94a9a8f698d88d1b92149ebc00e4a0ca834afab4
JLuisRojas <joseluisrojasaranda@gmail.com>
2021-11-29 18:38:16 -0600

    updated stack to new functions

--- lib/erp-bpm-cdk-stack.ts
+++ lib/erp-bpm-cdk-stack.ts
@@ -7,37 +7,40 @@ export class ErpBpmCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const api = new apigateway.RestApi(this, 'erp-bpm-api');
-
-    const layer = new lambda.LayerVersion(this, "erp-layer", {
-      code: new lambda.AssetCode("../erp-bpm-integration-api/layer/")
+    const api = new apigateway.RestApi(this, 'erp-bpm-api', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowCredentials: true,
+        allowMethods: apigateway.Cors.ALL_METHODS, // this is also the default
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
     });
 
     const fnTest = new lambda.Function(this, "test-func", {
-      layers: [layer],
+      // layers: [layer],
       timeout: Duration.seconds(30),
       runtime: lambda.Runtime.NODEJS_12_X,
-      handler: "handlers/test.handler",
+      handler: "test/handler.handler",
       memorySize: 512,
-      code: new lambda.AssetCode("../erp-bpm-integration-api/dist/"),
+      code: new lambda.AssetCode("../erp-bpm-integration-api/"),
     });
 
     const fnSend = new lambda.Function(this, "send-func", {
-      layers: [layer],
+      // layers: [layer],
       timeout: Duration.seconds(30),
       runtime: lambda.Runtime.NODEJS_12_X,
-      handler: "handlers/sendMessage.handler",
+      handler: "send-message/handler.handler",
       memorySize: 512,
-      code: new lambda.AssetCode("../erp-bpm-integration-api/dist/"),
+      code: new lambda.AssetCode("../erp-bpm-integration-api/"),
     });
 
     const fnStart = new lambda.Function(this, "start-func", {
-      layers: [layer],
+      // layers: [layer],
       timeout: Duration.seconds(30),
       runtime: lambda.Runtime.NODEJS_12_X,
-      handler: "handlers/startProccess.handler",
+      handler: "start-process/handler.handler",
       memorySize: 512,
-      code: new lambda.AssetCode("../erp-bpm-integration-api/dist/"),
+      code: new lambda.AssetCode("../erp-bpm-integration-api/"),
     });
 
     api.root.addMethod('ANY', new apigateway.LambdaIntegration(fnTest));
